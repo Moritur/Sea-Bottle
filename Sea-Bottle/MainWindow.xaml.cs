fd414cd8e6ea435b8275cc0e027755091a10ec2d
Moritur <44618240+Moritur@users.noreply.github.com>
2020-01-08 22:40:55 +0100

    Started working on CheckerBoard initiallization
    Started working on CheckerBoard initiallization and fixed typo in MainWindow.xaml

--- Sea-Bottle/MainWindow.xaml.cs
+++ Sea-Bottle/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace Sea_Bottle
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region configs
+        const int cellNumber = 100;
+        const int shotLimit = 40;
+        #endregion
+
+        (Image image, CellState state)[] cells = new (Image uiElement, CellState state)[cellNumber];
+
+        int clicksLeft = shotLimit;
+
+        enum CellState
+        {
+            empty,
+            ship,
+            miss,
+            hit,
+            destroyed
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeCells();
         }
+
+
+        private void InitializeCells()
+        {
+            for (int i = 0; i < cellNumber; i++)
+            {
+                int j = i;
+
+                Image image = new Image();
+                image.MouseDown += async (object sender, MouseButtonEventArgs e) => await HandleCellClickAsync(j);
+                CheckerBoard.Children.Add(image);
+
+                cells[i] = (image, CellState.empty);
+            }
+        }
+
+        async Task HandleCellClickAsync(int cellId)
+        {
+            if (cells[cellId].state == CellState.miss) return;
+
+            clicksLeft--;
+            await Task.Run(() => UpdateShipGridForClick(cellId));
+            UpdateCellUI(cellId);
+        }
+
+        void UpdateShipGridForClick(int cellId)
+        {
+            cells[cellId].state = (cells[cellId].state == CellState.ship) ? CellState.hit : CellState.miss;
+        }
+
+        void UpdateCellUI(int celllId)
+        {
+
+        }
+
     }
 }

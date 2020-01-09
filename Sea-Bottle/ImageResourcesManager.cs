2a117ae088af540455b99d0f13d3f31e7bb1e9ba
mariamajewska <48626106+mariamajewska@users.noreply.github.com>
2020-01-09 19:56:17 +0100

    Created manager for image resources
    Created class which loads images from resources

---
+++ Sea-Bottle/ImageResourcesManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeaBottle
+{
+    static class ImageResourcesManager
+    {
+        public static BitmapImage emptyCell = new BitmapImage();
+        public static BitmapImage miss = new BitmapImage();
+        public static BitmapImage hit = new BitmapImage();
+        public static BitmapImage destroyedShip  = new BitmapImage();
+        public static BitmapImage shot = new BitmapImage();
+
+        const string emptyCellUri;
+        const string missUri;
+        const string hitUri;
+        const string destroyedShipUri;
+        const string shotUri;
+
+        public static void Initialize()
+        {
+            InitBitmap(emptyCell, emptyCellUri);
+            InitBitmap(miss, missUri);
+            InitBitmap(hit, hitUri);
+            InitBitmap(destroyedShip, destroyedShipUri);
+            InitBitmap(shot, shotUri);
+        }
+
+        static void InitBitmap(BitmapImage bitmap, string uri)
+        {
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(uri, UriKind.Relative);
+            bitmap.EndInit();
+        }
+    }
+}

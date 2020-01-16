8f1f13694fc6f4804bfe6086d219ecaefe8dd9d0
50na50 <59621211+50na50@users.noreply.github.com>
2020-01-16 23:35:45 +0100

    Created Ship class

--- Sea-Bottle/Ship.cs
+++ Sea-Bottle/Ship.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Sea_Bottle
 {
-    struct Ship
+    class Ship
     {
+        public readonly IReadOnlyList<int> cells;
+        public bool isDestroyed { get; private set; }
+        public int Size => cells.Count;
+
+        public Ship(params int[] cellNumbers)
+        {
+            cells = Array.AsReadOnly(cellNumbers);
+            isDestroyed = false;
+        }
+
+        public void Destroy()
+        {
+            if (isDestroyed) throw new InvalidOperationException($"Ship positioned on cells: {cells.Aggregate("", (p, n) => p + " " + n.ToString())} was already destroyed");
+
+            isDestroyed = true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = int.MinValue;
+            for (int i = 0; i < cells.Count; i++)
+            {
+                hashCode += cells[i] * i;
+            }
+            return hashCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Ship)
+            {
+                return ((Ship)obj).cells.Equals(this.cells);
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
